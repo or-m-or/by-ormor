6605efca707fe3e17ff7daa4be316709a21b4443
ormor <htth815@gmail.com>
2025-01-22 04:07:26 +0900

    🍱 전역폰트 추가 및 설정(Pretendard)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+// src/app/page.tsx
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <main className="font-pretendard">
+      <h1 className="text-3xl font-bold">Pretendard 테스트 제목</h1>
+      <p className="font-light">Pretendard 테스트 글꼴</p>
+    </main>
+  );
 }
